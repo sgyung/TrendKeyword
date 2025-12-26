ebcd034fea860ed58fbd1eeb3195b04c2edf9e02
sgyung <tjsrud228@naver.com>
2025-12-26 17:59:44 +0900

    뉴스 제목 키워드 수집 수정

--- src/main/java/com/trendkeyword/collector/NewsCollector.java
+++ src/main/java/com/trendkeyword/collector/NewsCollector.java
@@ -1,27 +1,39 @@
 package com.trendkeyword.collector;
 
 import com.trendkeyword.api.naver.NaverNewsApiClient;
-import com.trendkeyword.collector.dto.NaverNewsItemDto;
 import com.trendkeyword.collector.dto.NaverNewsResponseDto;
+import com.trendkeyword.collector.keyword.KeywordExtractor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class NewsCollector {
 
     private final NaverNewsApiClient naverNewsApiClient;
+    private final KeywordExtractor keywordExtractor;
 
     @Async
-    public void collect(String query, int display, int start){
-        NaverNewsResponseDto response = naverNewsApiClient.searchNews(query, start, display);
+    public void collectLatestNews() {
 
-        for (NaverNewsItemDto item : response.getItems()) {
+        List<String> titles = new ArrayList<>();
 
-            String title = item.getTitle();
+        for (String query : NewsCollectionCategory.DEFAULT) {
+            NaverNewsResponseDto response =
+                    naverNewsApiClient.searchNews(query, 50, 1);
 
-            System.out.println("[NEWS TITLE] " + title);
+            response.getItems().forEach(item ->
+                    titles.add(item.getTitle()));
         }
+
+        // 👉 여기서 KeywordExtractor 호출
+        List<String> keywords =
+                keywordExtractor.extractKeywords(titles);
+
+        keywords.forEach(System.out::println);
     }
 }
